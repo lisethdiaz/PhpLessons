103c82e27f14a7bdfdc1e5989fa759d92b533371
Liseth Diaz <110925212+lisethdiaz@users.noreply.github.com>
2024-03-21 08:35:16 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="GET">
+        <input type="text" name="person">
+        <button>SUBMIT</button>
+</form>
+    <?php 
+        $name = $_GET ['person'];
+        echo $name. " is kind of fun";
+    ?>
+</body>
+</html>
