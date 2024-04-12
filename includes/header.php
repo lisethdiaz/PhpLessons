283a361c247f7ef6f28af9645135e3b67fccf1bd
Liseth Diaz <110925212+lisethdiaz@users.noreply.github.com>
2024-04-12 07:38:28 -0500

    push

--- includes/header.php
+++ includes/header.php
@@ -45,6 +45,16 @@
   </div>
 </nav>
 
+<ul>
+  <li><a href="index.php">HOME</a></li>
+  <li><a href="contact.php">CONTACT</a></li>
+</ul>
+
+<?php
+
+
+
+?>
 
     <h1>Hello, world!</h1>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
